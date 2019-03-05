ea8d3b3ad6e7aa6c8a9331175ffa70228929d628
vixuelo <vixuelo@users.noreply.github.com>
2019-03-05 13:55:30 +0100

    https://code.tutsplus.com/es/tutorials/how-to-create-an-android-chat-app-using-firebase--cms-27397

---
+++ app/src/main/java/es/centroafuera/intentochatfb/MainActivity.java
@@ -0,0 +1,17 @@
+package es.centroafuera.intentochatfb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    }
+}
